bcd0cd21a81c1587ec66ab8d822a418009ddacd0
Matheus Glauber <matheus.ribeiro13@fatec.sp.gov.br>
2020-04-28 12:00:44 -0300

    Projeto iniciado, projeto em branco

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PROJETO_XADREZ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
